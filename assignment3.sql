4588f2ef431aec241ca2dd7b806e9559449a74bb
sophisticated21 <90183964+sophisticated21@users.noreply.github.com>
2022-11-28 19:27:43 +0100

    Create assignment3.sql

---
+++ assignment3.sql
@@ -0,0 +1,21 @@
+--assignment3
+
+--question1
+SELECT country
+FROM country
+WHERE country LIKE 'A%a';
+
+--question2
+SELECT country
+FROM country
+WHERE country LIKE '%_____n';
+
+--question3
+SELECT title
+FROM film
+WHERE title ILIKE '%t%t%t%t%';
+
+--question4
+SELECT *
+FROM film
+WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
