d1322ccdf5036f8bf00035c1377f12e4daa0015c
sophisticated21 <90183964+sophisticated21@users.noreply.github.com>
2022-11-29 09:17:29 +0100

    Create assignment5.sql

---
+++ assignment5.sql
@@ -0,0 +1,22 @@
+--assignment5
+
+--question1
+SELECT *
+FROM film 
+WHERE title LIKE '%n'
+ORDER BY length DESC
+LIMIT 5;
+
+--question2
+SELECT *
+FROM film 
+WHERE title LIKE '%n'
+ORDER BY length 
+OFFSET 5
+LIMIT 5;
+--question3
+SELECT *
+FROM customer
+WHERE store_id = 1
+ORDER BY last_name DESC
+LIMIT 4;
